f0420def29dd880ce4dce12ec4053af613160fb9
songyanzf <762350299@qq.com>
2016-03-04 11:22:12 +0800

    第七周ajax

--- 前五周/20160114_全日制第二周第三天/js/tab.js
+++ 前五周/20160114_全日制第二周第三天/js/tab.js
@@ -9,21 +9,19 @@ function bindData() {
     for (var i = 0; i < jsonAry.length; i++) {
         var cur = jsonAry[i];
         cur.sex = cur.sex === 0 ? "男" : "女";
-
         var oTr = document.createElement("tr");
-
         for (var key in cur) {
             var oTd = document.createElement("td");
             oTd.innerHTML = cur[key];
             oTr.appendChild(oTd);
         }
-
         frg.appendChild(oTr);
     }
     tBody.appendChild(frg);
     frg = null;
 }
 bindData();
+
 
 function changeBg() {
     for (var i = 0; i < oTrs.length; i++) {
